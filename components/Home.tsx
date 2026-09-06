cdee362b996a38210de1451f6405fb1a7ccf692e
asia272 <asiaashraf7272@gmail.com>
2026-09-06 11:02:15 +0500

    add basic UI

---
+++ components/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import LeftPanel from './LeftPanel'
+import RightPanel from './RightPanel'
+
+const Home = () => {
+    return (
+        <main className='m-5'>
+            <div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
+                {/* Green background decorator for Light Mode */}
+                <div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
+                <LeftPanel />
+                <RightPanel />
+            </div>
+        </main>
+    )
+}
+
+export default Home
