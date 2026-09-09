da1b930cbb58253862d44b3a068238c798a9c920
asia272 <asiaashraf7272@gmail.com>
2026-09-09 13:34:25 +0500

    zustand global state added

--- components/home/LeftPanel.tsx
+++ components/home/LeftPanel.tsx
@@ -1,17 +1,19 @@
 import { ListFilter, MessageSquareDiff, Search, } from "lucide-react";
 import { Input } from "../ui/input";
 import ThemeSwitch from "../ThemeSwitch";
-import { conversations } from "@/dummy-data/db";
 import Conversation from "../Conversation";
 import { UserButton } from "@clerk/nextjs";
 import UserListDialog from "../user-list-dialog";
-import { useConvexAuth } from "convex/react";
+import { useConvexAuth, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 
 
 const LeftPanel = () => {
 
+
     const { isAuthenticated } = useConvexAuth();
+    const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
 
     return (
         <div className='w-1/4 border-gray-600 border-r'>
