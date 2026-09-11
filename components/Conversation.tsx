3a98cbc7de226548af4e39d2676251ba7a74a003
asia272 <asiaashraf7272@gmail.com>
2026-09-11 12:06:47 +0500

    fix converstain last message type

--- components/Conversation.tsx
+++ components/Conversation.tsx
@@ -40,18 +40,34 @@ const Conversation = ({ conversation }: { conversation: any }) => {
                             {formatDate(lastMessage?._creationTime || conversation._creationTime)}
                         </span>
                     </div>
-                    <p className='text-[12px] mt-1 text-gray-500 flex items-center gap-1 '>
-                        {lastMessage?.sender === me?._id ? <MessageSeenSvg /> : ""}
+
+                    <p className="text-[12px] mt-1 text-gray-500 flex items-center gap-1">
+                        {lastMessage?.sender === me?._id && <MessageSeenSvg />}
+
                         {conversation.isGroup && <Users size={16} />}
-                        {!lastMessage && "Say Hi!"}
-                        {lastMessageType === "text" && lastMessage?.content.length > 30 ? (
-                            <span className='text-xs'>{lastMessage?.content.slice(0, 30)}...</span>
-                        ) : (
-                            <span className='text-xs'>{lastMessage?.content}</span>
-                        )}
-                        {lastMessageType === "image" && <ImageIcon size={16} />}
-                        {lastMessageType === "video" && <VideoIcon size={16} />}
+
+                        {!lastMessage ? (
+                            "Say Hi!"
+                        ) : lastMessageType === "text" ? (
+                            <span className="text-xs">
+                                {lastMessage.content.length > 30
+                                    ? `${lastMessage.content.slice(0, 30)}...`
+                                    : lastMessage.content}
+                            </span>
+                        ) : lastMessageType === "image" ? (
+                            <>
+                                <ImageIcon size={16} />
+                                <span className="text-xs">Photo</span>
+                            </>
+                        ) : lastMessageType === "video" ? (
+                            <>
+                                <VideoIcon size={16} />
+                                <span className="text-xs">Video</span>
+                            </>
+                        ) : null}
                     </p>
+
+
                 </div>
             </div >
             <hr className='h-[1px] mx-10 bg-gray-primary' />
